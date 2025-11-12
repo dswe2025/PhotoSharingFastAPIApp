70488b03cec27c0d37c1c06ac4281b3cc69652ed
d3 <d3@d3s-MacBook-Pro.local>
2025-11-12 01:29:49 -0800

    Upload and commit new post

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
-
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -17,13 +17,50 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post("/upload()")
+@app.post("/upload")
 async def upload_file(
     file: UploadFile = File(...),
     caption: str = Form(""),
     session: AsyncSession = Depends(get_async_session)
 ):
-    pass
+    post = Post(
+        caption=caption,
+        url="dummy url",
+        file_type="photo",
+        file_name="dummy name"
+    )
+
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post 
+
+#Feed
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    #this query the data from the database
+    #select from the post, then you have the filter ordered by descending.
+
+    #Prepare for the frontend
+    posts_data = []
+    for post in posts:
+        #create a more response post on the frontend
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption":post.caption,
+                "url":post.url,
+                "file_type":post.file_type,
+                "file_name":post.created_at.isoformat(),
+                "created_ad":post.created_at.isoformat()
+            }
+        )
+    return {"posts": posts_data}
     
 
 # # #Hello World
